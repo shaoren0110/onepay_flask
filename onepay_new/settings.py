7ee77f21ba7b5fb9cb2b3f489de8854f2af3d148
sam_new <sam.shao@tedcall.com>
2018-12-18 17:03:50 +0800

    1.增加logging配置

--- onepay_new/settings.py
+++ onepay_new/settings.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+import logging
 
 # sqlite URI compatible
 WIN = sys.platform.startswith('win')
@@ -16,3 +17,8 @@ dev_db = prefix + os.path.join(basedir, 'data.db')
 SECRET_KEY = os.getenv('SECRET_KEY', 'secret string')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', dev_db)
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='[%(asctime)s.%(msecs)03d]: %(message)s',
+                    datefmt='%Y-%m-%d %X',
+                    stream=sys.stdout)
